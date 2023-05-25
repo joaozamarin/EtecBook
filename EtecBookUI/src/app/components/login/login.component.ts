90b23adbcd81d9d881eedb57c2b96ddb38b01e2f
joaozamarin <joao.zamarin@gmail.com>
2023-05-25 20:36:30 -0300

    validando form login e registro

--- EtecBookUI/src/app/components/login/login.component.ts
+++ EtecBookUI/src/app/components/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -11,12 +11,42 @@ export class LoginComponent implements OnInit {
 
   constructor(
     private fb: FormBuilder
-  ) {}
+  ) { }
 
   ngOnInit() {
     this.loginForm = this.fb.group({
       email: ['', Validators.required],
       password: ['', Validators.required]
     });
+  }
+
+  checkEmail(): boolean {
+    return this.loginForm.controls['email'].dirty && this.loginForm.hasError('required', 'email');
+  }
+
+  checkPassword(): boolean {
+    return this.loginForm.controls['password'].dirty && this.loginForm.hasError('required', 'password');
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      // Enviar os dados ao backend
+      console.log(this.loginForm.value);
+    } else {
+      // Dispara erro
+      this.validateAllFormFields(this.loginForm);
+    }
+  }
+
+  private validateAllFormFields(formGroup: FormGroup) {
+    Object.keys(formGroup.controls).forEach(field => {
+      const control = formGroup.get(field);
+
+      if (control instanceof FormControl) {
+        control.markAsDirty({ onlySelf: true })
+      } else if (control instanceof FormGroup) {
+        this.validateAllFormFields(control);
+      }
+    })
   }
 }
--- EtecBookUI/src/app/components/register/register.component.ts
+++ EtecBookUI/src/app/components/register/register.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  registroForm!: FormGroup;
 
+  constructor(
+    private fb: FormBuilder
+  ) { }
+
+  ngOnInit() {
+    this.registroForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      password: ['', Validators.compose([Validators.required, Validators.minLength(6)])]
+    });
+  }
+
+  checkName(): boolean {
+    return this.registroForm.controls['name'].dirty && this.registroForm.controls['name'].errors?.['required'];
+  }
+
+  checkEmail(): boolean {
+    return this.registroForm.controls['email'].dirty && this.registroForm.controls['email'].errors?.['required'];
+  }
+
+  checkEmailValid(): boolean {
+    return this.registroForm.controls['email'].dirty && this.registroForm.controls['email'].errors?.['email'] && this.registroForm.controls['email'].touched;
+  }
+
+  checkPassword(): boolean {
+    return this.registroForm.controls['password'].dirty && this.registroForm.controls['password'].errors?.['required'];
+  }
+
+  checkPasswordLength(): boolean {
+    return this.registroForm.controls['password'].dirty && this.registroForm.controls['password'].errors?.['minlength'] && this.registroForm.controls['password'].touched;
+  }
+
+  onSubmit() {
+    if (this.registroForm.valid) {
+      // Enviar os dados ao backend
+      console.log(this.registroForm.value);
+    } else {
+      // Dispara erro
+      this.validateAllFormFields(this.registroForm);
+    }
+  }
+
+  private validateAllFormFields(formGroup: FormGroup) {
+    Object.keys(formGroup.controls).forEach(field => {
+      const control = formGroup.get(field);
+
+      if (control instanceof FormControl) {
+        control.markAsDirty({ onlySelf: true })
+      } else if (control instanceof FormGroup) {
+        this.validateAllFormFields(control);
+      }
+    })
+  }
 }
